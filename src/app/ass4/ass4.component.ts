5eff92bad6099ddbcb23c58a07dd703270e1d6f7
Akshay Kumar Nayak <akshaykmr286@gmail.com>
2017-09-06 16:34:07 +0530

    my second commit

--- src/app/ass4/ass4.component.ts
+++ src/app/ass4/ass4.component.ts
@@ -12,23 +12,22 @@ export class Ass4Component implements OnInit {
  ismenter:boolean = false;
  ismleave:boolean = false;
  color;
+ selectedindex;
   ngOnInit() {
     /*INITIAALIZING ARRAY*/
     this.items = ["Akshay","Atish","Mayank","Ashir","Manish"];
     this.color = 'black';
   }
 MouseEnter(index){
+  this.selectedindex = index;
   /*checking odd or even*/
   if(index % 2 == 0){
-    //document.getElementById(index).style.color = 'green';
     this.color = 'green';
   }else{
-    //document.getElementById(index).style.color = 'red';
     this.color = 'red';
   }
 }
 MouseLeave(index){
-   //document.getElementById(index).style.color = 'black';
     this.color = 'black';
 }
 }
